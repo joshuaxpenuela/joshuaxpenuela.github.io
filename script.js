33dcfaff869b5fc2c0bb789e95e45542425c3d78
Joshua Penuela <123760171+joshuaxpenuela@users.noreply.github.com>
2023-03-11 04:38:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["IT Student", "Information Technology Student"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+})
